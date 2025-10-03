48d3b2f647e4764dea6cfc954cb43fd7ff5780fe
Emmanuel Sunday <ebubesunday16@gmail.com>
2025-10-04 00:35:05 +0100

    Update Introduction.tsx

--- component/LandingPage/Introduction.tsx
+++ component/LandingPage/Introduction.tsx
@@ -30,13 +30,12 @@ const Introduction = () => {
       className='bg-[#faeadc] h-[300vh]'
     >
       <motion.div 
-      initial={{ scale: 0.9, opacity: 0 }}
-      animate={isInView ? { scale: 1, opacity: 1, height: "100vh" } : { scale: 0.9, opacity: 0.3 }}
-      transition={{ duration: 0.8, ease: "easeOut" }}
+      
       className='h-screen sticky top-0 bg-brand-black rounded-t-[16px] flex flex-col items-start justify-center gap-y-12 px-4'
       style={{
         borderTopLeftRadius: useTransform(scrollYProgress, [0, 0.067, 0.134, 0.2, 0.4], [72, 16, 16, 10.66,  0]),
         borderTopRightRadius: useTransform(scrollYProgress, [0, 0.067, 0.134, 0.2, 0.4 ], [72, 16, 16, 10.66, 0]),
+        y: useTransform(scrollYProgress, [0, 0.134, 0.4], [0, -100, 0 ]),
         
         
         
@@ -45,7 +44,7 @@ const Introduction = () => {
       >
           <div className='self-center flex gap-2 items-center'>
             <LabelOne />
-            <p className='rounded-[12px] border border-[#FBF0E6]text-branded-black font-semibold text-base text-[#FBF0E6] py-2 px-2.5'>Introduction</p>
+            <p className='rounded-[12px] border border-[#FBF0E6]text-branded-black font-semibold text-sm text-[#FBF0E6] py-2 px-2.5'>Introduction</p>
 
           </div>
             <Image 
@@ -62,7 +61,7 @@ const Introduction = () => {
               <span className='text-2xl font-champBlack text-brand-white'>I’m Emmanuel Sunday, <span className='bg-[#8BDFDD] text-brand-black px-2 rounded-[8px] '>Software</span> Engineer,  and CS Junior Student.</span>
             </div>
 
-            <div className='space-y-4 text-brand-white'>
+            <div className='space-y-4 text-brand-white text-sm'>
               <p>I design and build websites and applications that are beautiful, structured, and secure by default.</p>
               <p>With a BSc in Computer Science and over 3 years                   of hands-on experience, I bring structure, technical expertise and design sense to every project i work on. These are my manthras!</p>
             </div>
