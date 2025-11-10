441e711b2a856b58e5d65b9d172f7367fde3c298
Emmanuel Sunday <ebubesunday16@gmail.com>
2025-11-10 16:08:54 +0100

    improve speed of animation, added google spatial animation effect

--- component/LandingPage/ZoomZoom.tsx
+++ component/LandingPage/ZoomZoom.tsx
@@ -15,6 +15,11 @@ const ZoomZoom = () => {
   // useMotionValueEvent(scrollYProgress, "change", (latest) => {
   //   console.log("scrollYProgress", latest)
   // })
+  const expressiveSpatial = {
+    stiffness: 280,  
+    damping: 20,    
+    mass: 0.8,  
+  };
 
   const parent = {
     hidden: {},
@@ -30,6 +35,10 @@ const ZoomZoom = () => {
     hidden: { scale: 0, opacity: 0 },
     show: { scale: 1, opacity: 1 }
   }
+
+  useMotionValueEvent(scrollYProgress, 'change', (latest) => {
+    console.log('scrolly progress for zoom zoom', latest)
+  } )
 
   return (
     <motion.div 
@@ -63,13 +72,11 @@ const ZoomZoom = () => {
               damping: 20
             }}
             style={{
-              y: useTransform(scrollYProgress, [0.25, 0.3, 0.35], [0, -100, -120]),
-              x: useTransform(scrollYProgress, [0.25, 0.27, 0.35, 0.65], [0, 70, 100, 120]),
-              scale: useTransform(scrollYProgress, [0.4, 0.55], [1, 0.4]),
-              borderRadius: useTransform(scrollYProgress, [0.6, 0.65], [16, 1000]),
+              y: useSpring(useTransform(scrollYProgress, [0.25, 0.27, 0.35], [0, -100, -120]), expressiveSpatial),
+              x: useSpring(useTransform(scrollYProgress, [0.25, 0.27, 0.35, 0.46], [0, 70, 100, 120]), expressiveSpatial),
+              scale: useSpring(useTransform(scrollYProgress, [0.4, 0.45 ], [1, 0.4]), expressiveSpatial),
+              borderRadius: useSpring(useTransform(scrollYProgress, [0.4, 0.55], [16, 1000]), expressiveSpatial),
             }}
-            
-            
             >
               <Zoom />
             </motion.div>
@@ -77,15 +84,11 @@ const ZoomZoom = () => {
             <motion.div 
             className='bg-[#CD5334] flex items-center justify-center w-25 h-25 rounded-[16px] absolute translate-x-20 -translate-y-4 bottom-12 transform scale-160'
             variants={child}
-            transition={{
-              type: 'spring',
-              stiffness: 120,
-              damping: 20
-            }}
+            
             style={{
-              y: useTransform(scrollYProgress, [0.25, 0.35, 0.7], [0, 10, 40]),
-              x: useTransform(scrollYProgress, [0.25, 0.35, 0.7], [0, 0, -10]),
-              scale: useTransform(scrollYProgress, [0.4, 0.55], [1, 0.25])
+              y: useSpring(useTransform(scrollYProgress, [0.25, 0.26, 0.35, 0.5], [0, 8, 10, 40]), expressiveSpatial),
+              x: useSpring(useTransform(scrollYProgress, [0.25, 0.26, 0.35], [0, 0, -4]), expressiveSpatial),
+              scale: useSpring(useTransform(scrollYProgress, [0.4, 0.45], [1, 0.25]), expressiveSpatial)
             }}
             
             >
@@ -102,9 +105,9 @@ const ZoomZoom = () => {
               damping: 20
             }}
             style={{
-              y: useTransform(scrollYProgress, [0.25, 0.35, 0.759, 0.8], [0, -30, -30, 8]),
-              x: useTransform(scrollYProgress, [0.25, 0.35, 0.8, 0.82], [0, -60, -60, -48]),
-              scale: useTransform(scrollYProgress, [0.4, 0.55], [1, 0.286]),
+              y: useSpring(useTransform(scrollYProgress, [0.25, 0.26, 0.3, 0.7, 0.71], [0, -5, -30, -30, 8]), expressiveSpatial),
+              x: useSpring(useTransform(scrollYProgress, [0.25, 0.26, 0.3, 0.6, 0.72], [0, -15, -60, -60, -48]), expressiveSpatial),
+              scale: useSpring(useTransform(scrollYProgress, [0.4, 0.45], [1, 0.286]), expressiveSpatial),
               borderRadius: useTransform(scrollYProgress, [0.4, 0.55], [16, 1000])
             }}
             >
@@ -117,9 +120,9 @@ const ZoomZoom = () => {
           <motion.div 
           className='mx-auto items-center text-center text-brand-white font-champBlack space-y-5 sticky top-0 text-4xl'
           style={{
-            y: useTransform(scrollYProgress, [0.45, 0.6], [0, 300]),
-            opacity: useTransform(scrollYProgress, [0.45, 0.48], [1, 0]),
-            display: useTransform(scrollYProgress, [0.48, 0.488], ['block', 'hidden']),
+            y: useTransform(scrollYProgress, [0.35, 0.5], [0, 300]),
+            opacity: useTransform(scrollYProgress, [0.4, 0.43], [1, 0]),
+            display: useTransform(scrollYProgress, [0.4, 0.43], ['block', 'hidden']),
           }}
           >
             <p> The software engineer you <span className='text-[#eca5c9]'>wish</span> you hired</p>
@@ -128,46 +131,46 @@ const ZoomZoom = () => {
           <motion.div 
           className=' mx-auto items-center text-center text-white  font-champBlack space-y-5  text-4xl'
           style={{
-            y: useTransform(scrollYProgress, [0.595, 0.61], [0, -530]),
-            opacity: useTransform(scrollYProgress, [0.595, 0.61], [0, 1])
+            y: useTransform(scrollYProgress, [0.48, 0.485], [0, -530]),
+            opacity: useTransform(scrollYProgress, [0.48, 0.5], [0, 1])
           }}
           
           >
             <motion.span 
               className='block'
               style={{
-                opacity: useTransform(scrollYProgress, [0.63, 0.65], [0, 1]),
-                x: useTransform(scrollYProgress, [0.6, 0.65], [-40, 1]),
+                opacity: useSpring(useTransform(scrollYProgress, [0.5, 0.52], [0, 1]), expressiveSpatial),
+                x: useSpring(useTransform(scrollYProgress, [0.49, 0.52], [-40, 1]), expressiveSpatial),
 
               }}
               > I </motion.span>
             <motion.span className='block'
             style={{
-              opacity: useTransform(scrollYProgress, [0.63, 0.7], [0, 1]),
-              x: useTransform(scrollYProgress, [0.63, 0.7], [40, 1]),
+              opacity: useSpring(useTransform(scrollYProgress, [0.52, 0.55], [0, 1]), expressiveSpatial),
+              x: useSpring(useTransform(scrollYProgress, [0.51, 0.55], [40, 1]), expressiveSpatial),
 
             }}
             >turn</motion.span>
             <motion.span
              className='block'
              style={{
-              opacity: useTransform(scrollYProgress, [0.65, 0.72], [0, 1]),
-              x: useTransform(scrollYProgress, [0.65, 0.72], [-40, 1]),
+              opacity: useTransform(scrollYProgress, [0.55, 0.6], [0, 1]),
+              x: useTransform(scrollYProgress, [0.54, 0.6], [-40, 1]),
 
             }}
              >ideas</motion.span>
             <motion.span className='block'
             style={{
-              opacity: useTransform(scrollYProgress, [0.69, 0.74], [0, 1]),
-              x: useTransform(scrollYProgress, [0.69, 0.74], [40, 1]),
+              opacity: useSpring(useTransform(scrollYProgress, [0.6, 0.65], [0, 1]), expressiveSpatial),
+              x: useSpring(useTransform(scrollYProgress, [0.59, 0.65], [40, 1]), expressiveSpatial),
 
             }}
             >to</motion.span>
             <motion.span 
             className='block'
             style={{
-              opacity: useTransform(scrollYProgress, [0.74, 0.76], [0, 1]),
-              x: useTransform(scrollYProgress, [0.74, 0.76], [40, 1]),
+              opacity: useSpring(useTransform(scrollYProgress, [0.65, 0.7], [0, 1]), expressiveSpatial),
+              x: useSpring(useTransform(scrollYProgress, [0.64, 0.7], [40, 1]), expressiveSpatial),
             }}
             >businesses</motion.span>
 
