d98583e7c44c93034c1a94d6555cad433882c31e
Emmanuel Sunday <ebubesunday16@gmail.com>
2025-11-15 10:56:29 +0100

    Update FeaturedProject.tsx

--- component/LandingPage/FeaturedProject.tsx
+++ component/LandingPage/FeaturedProject.tsx
@@ -116,10 +116,10 @@ const FeaturedProject = () => {
             href="https://soapnotes.doctor"
             target="_blank"
             rel="noopener noreferrer"
-            className="cursor-pointer bg-black text-white px-4 py-2 rounded"
+            className="cursor-pointer"
           >
 
-              <Link  className='text-[#fdf9f0] '/>
+              <Link />
           </a>
 
       </div>
